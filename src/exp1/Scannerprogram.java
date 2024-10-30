d0ca601fd70ed426093321947b6e0f553870b6de
BRSREDDY <BRSREDDY@DESKTOP-SCON867>
2024-10-30 18:50:44 +0530

    commited success

---
+++ src/exp1/Scannerprogram.java
@@ -0,0 +1,10 @@
+package exp1;
+
+public class Scannerprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/exp1/calculator.java
@@ -0,0 +1,26 @@
+package exp1;
+
+
+
+
+public class calculator {
+
+	public void sum(){
+		
+		int a;
+		int b;
+		int z;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+	}
+
+}
